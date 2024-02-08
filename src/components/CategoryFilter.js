4727f20ff56c4f9f19c01a1592f689c773b07016
bradleymbuvi <bradwent@gmail.com>
2024-02-08 15:28:14 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
+
+function CategoryFilter({ categories: category, filterTasks }) {
+	const [active, setActive] = useState("All");
+	return (
+		<div className="categories">
+			<h5>Category filters</h5>
+			{category.map((user) => (
+				<button
+					className={user === active ? "selected" : ""}
+					key={user}
+					onClick={() => {
+						filterTasks(user);
+						setActive(user);
+					}}
+				>
+					{user}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryFilter;
